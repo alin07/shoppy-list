44db774a93ebf112f423a91fc0c3514f543bb709
Alice <lin.alice72@gmail.com>
2024-11-14 23:03:28 -0500

    fixed max duration config syntax for /pages

--- pages/api/recipe.ts
+++ pages/api/recipe.ts
@@ -1,7 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import puppeteer from 'puppeteer-core';
 import chromium from '@sparticuz/chromium-min';
-export const maxDuration = 60;
+
+export const config = {
+  maxDuration: 5,
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { url } = req.query;
 
