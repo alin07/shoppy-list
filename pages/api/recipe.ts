b69efa41d0f740623457c75d9b912eb8b446b0cd
Alice <lin.alice72@gmail.com>
2024-11-14 22:56:45 -0500

    max duration vercel config

--- pages/api/recipe.ts
+++ pages/api/recipe.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import puppeteer from 'puppeteer-core';
 import chromium from '@sparticuz/chromium-min';
-
+export const maxDuration = 60;
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { url } = req.query;
 
